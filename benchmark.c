f81277c279f846213d7ee50c38f3b7d9f48c06eb
Amit <amitpay1234@gmail.com>
2022-12-09 01:24:26 +0200

    check sum function implemented

--- benchmark.c
+++ benchmark.c
@@ -3,10 +3,35 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+/**
+ * Check Sum Function.
+ * Input-> String of file location.
+ * Output-> checksum of file.
+ * @param file_location
+ * @return
+ */
+unsigned int checksum(char * file_location)
+{
+    FILE * fp= fopen(file_location,"rb");
+    unsigned char checksum = 0;
+    while (!feof(fp) && !ferror(fp))
+    {
+        checksum ^= fgetc(fp);
+    }
+    fclose(fp);
+    return checksum;
+}
+
+
 
 
 int main(){
     //File to open
+
     FILE *fp= fopen("text.txt","wb");
     if(fp==NULL)
     {
@@ -17,7 +42,8 @@ int main(){
     long random_file_size = 100000000;
     //Generate file
     for (int i = 0; i < random_file_size; ++i) {
-        int num=(rand()%126);
-        fputc(num,fp);
+        int num=(rand()%2);
+        fputc(num+48,fp);
     }
+    printf("%d", checksum("text.txt"));
 }
